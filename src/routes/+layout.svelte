eb6b1c619d3f8bcb139bb08776f23e71ace76bf8
momo391 <ia7636hs@gmail.com>
2024-10-17 20:27:41 +0100

    shadcn as ui library with dark mode using mode watcher

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,12 @@
 <script>
+	import { ModeWatcher } from 'mode-watcher';
 	import '../app.css';
 	import '@fontsource-variable/overpass';
 
 	let { children } = $props();
 </script>
 
+<ModeWatcher />
 <div class="font-sans min-h-screen">
 	{@render children()}
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import ModeToggle from '$lib/components/mode-toggle.svelte';
+</script>
+
+<main class="min-h-screen flex items-center justify-center">
+	<ModeToggle />
+</main>
