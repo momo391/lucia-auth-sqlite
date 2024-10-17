4ac2cf97b03703c2f72abdab3f641ff8fff381de
momo391 <ia7636hs@gmail.com>
2024-10-17 19:56:08 +0100

    styles and fonts

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import '../app.css';
+	import '@fontsource-variable/overpass';
+
+	let { children } = $props();
+</script>
+
+<div class="font-sans min-h-screen">
+	{@render children()}
+</div>
